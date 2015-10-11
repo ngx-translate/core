841f6eb4c530a2f39d425ac731bb3d22d8b5bd81
Olivier Combe <olivier.combe@gmail.com>
2015-10-11 12:08:46 +0200

    distributing typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="index.ts" />;
+declare module 'ng2-translate' {
+    export * from 'index';
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+import {TranslatePipe} from './src/translate.pipe';
+import {TranslateService} from './src/translate.service';
+
+export {
+    TranslatePipe,
+    TranslateService
+};
