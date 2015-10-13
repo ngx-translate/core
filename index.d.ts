6dd2b58aa29cdb417a06493f70b49f55140c560a
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-10-13 11:18:01 +0300

    chore(build): ignore generated files

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="index.ts" />;
-
-declare module 'ng2-translate' {
-    import index = require("index");
-    export = index;
-}
--- index.js
+++
@@ -1,4 +0,0 @@
-var translate_pipe_1 = require('./src/translate.pipe');
-exports.TranslatePipe = translate_pipe_1.TranslatePipe;
-var translate_service_1 = require('./src/translate.service');
-exports.TranslateService = translate_service_1.TranslateService;
