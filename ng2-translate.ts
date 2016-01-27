edf67bf11e1f18d9e585c2bdc0cea7e7a27fffbf
Nathan Walker <walkerrunpdx@gmail.com>
2016-01-26 23:57:49 -0800

    feat(cli): compatible with angular cli using 'ng install'

--- ng2-translate.ts
+++ ng2-translate.ts
@@ -1,3 +1,11 @@
+import {TranslatePipe} from './src/translate.pipe';
+import {TranslateService} from './src/translate.service';
+
 export * from './src/translate.pipe';
 export * from './src/translate.service';
 export * from './src/translate.parser';
+
+export default {
+  pipes: [TranslatePipe],
+  providers: [TranslateService]
+}
