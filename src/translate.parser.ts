692dccc00e1773b1bcd92cf75558e1acd2b00446
Ulmann Christian <christian.ulmann@bit.admin.ch>
2016-03-17 08:42:19 +0100

    fix(TranslateParser): allow keys with dots along with subkeys
    Fixes #56

--- src/translate.parser.ts
+++ src/translate.parser.ts
@@ -32,7 +32,7 @@ export class Parser {
         key = '';
         do {
             key += keys.shift();
-            if (target[key]) {
+            if (target[key] && (typeof target[key] === 'object' || !keys.length)) {
                 target = target[key];
                 key = '';
             } else if (!keys.length) {
