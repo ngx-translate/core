7ae55e2cbd57c282774476d70fde465300382d65
Olivier Combe <olivier.combe@gmail.com>
2016-08-20 16:56:17 +0200

    chore(examples): adding an ionic 2 example

---
+++ examples/ionic2/app/pages/about/about.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/about/about.html'
+})
+export class AboutPage {
+  constructor(private navCtrl: NavController) {
+  }
+}
---
+++ examples/ionic2/app/pages/contact/contact.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/contact/contact.html'
+})
+export class ContactPage {
+  constructor(private navCtrl: NavController) {
+  }
+}
