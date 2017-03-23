c27e2777126bb4feea9666160fc15b8309a1fa9c
Olivier Combe <olivier.combe@gmail.com>
2017-03-23 21:25:42 +0100

    chore(npm): fix warnings/errors due to update to v4

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -63,7 +63,7 @@ module.exports = {
     plugins: [
         // fix the warning in ./~/@angular/core/src/linker/system_js_ng_module_factory_loader.js
         new webpack.ContextReplacementPlugin(
-            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            /angular(\\|\/)core(\\|\/)@angular/,
             helpers.root('./src')
         ),
 
