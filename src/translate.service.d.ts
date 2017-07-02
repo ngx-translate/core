16b0a8e9336433b820780492ff570580534a5f88
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-03 06:40:03 +0800

    added type definition of the overloaded get result

---
+++ src/translate.service.d.ts
@@ -0,0 +1,6 @@
+export declare class TranslateService {
+
+    get(key: Array<string>, interpolateParams?: Object): Observable<any>;
+    get(key: string, interpolateParams?: Object): Observable<string>;
+
+}
