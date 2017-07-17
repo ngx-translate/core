3017bde39ed663d87d66bb90ee60956de1177ff8
Esteban Marin <estebanmarin@gmx.ch>
2017-07-17 10:53:53 +0200

    Update translate.service.ts

--- src/translate.service.ts
+++ src/translate.service.ts
@@ -5,8 +5,8 @@ import "rxjs/add/observable/of";
 import "rxjs/add/operator/concat";
 import "rxjs/add/operator/share";
 import "rxjs/add/operator/map";
-import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/merge";
+import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/toArray";
 import "rxjs/add/operator/take";
 
