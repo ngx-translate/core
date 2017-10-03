b72bd8236095675daba7c3b6d2b36d50bfe89af6
Dmitry Baranovsky <dbaranovsky@baranovskyd-2.local>
2017-10-03 23:02:17 +0100

    #692: fix 'browserLang is undefined' error, returning undefined from getBrowserLang method

--- src/translate.service.ts
+++ src/translate.service.ts
@@ -534,6 +534,10 @@ export class TranslateService {
         let browserLang: any = window.navigator.languages ? window.navigator.languages[0] : null;
         browserLang = browserLang || window.navigator.language || window.navigator.browserLanguage || window.navigator.userLanguage;
 
+        if(!browserLang) {
+            return undefined;
+        }
+
         if(browserLang.indexOf('-') !== -1) {
             browserLang = browserLang.split('-')[0];
         }
