23bf3e5eaa6a2aac24a9e709821f636483e070aa
trik <marco.marche@gnucoop.com>
2019-02-01 18:13:20 +0100

    fix(TranslatePipe): subscriptions are assigned to EventEmitter props
    make all subscriptions private, no need to expose them

--- projects/ngx-translate/core/src/lib/translate.pipe.ts
+++ projects/ngx-translate/core/src/lib/translate.pipe.ts
@@ -1,5 +1,8 @@
-import {ChangeDetectorRef, EventEmitter, Injectable, OnDestroy, Pipe, PipeTransform} from '@angular/core';
-import {DefaultLangChangeEvent, LangChangeEvent, TranslateService, TranslationChangeEvent} from './translate.service';
+import {ChangeDetectorRef, Injectable, OnDestroy, Pipe, PipeTransform} from '@angular/core';
+
+import {Subscription} from 'rxjs'
+
+import {LangChangeEvent, TranslateService, TranslationChangeEvent} from './translate.service';
 import {equals, isDefined} from './util';
 
 @Injectable()
@@ -11,9 +14,10 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
   value: string = '';
   lastKey: string;
   lastParams: any[];
-  onTranslationChange: EventEmitter<TranslationChangeEvent>;
-  onLangChange: EventEmitter<LangChangeEvent>;
-  onDefaultLangChange: EventEmitter<DefaultLangChangeEvent>;
+
+  private _onTranslationChangeSub: Subscription;
+  private _onLangChangeSub: Subscription;
+  private _onDefaultLangChangeSub: Subscription;
 
   constructor(private translate: TranslateService, private _ref: ChangeDetectorRef) {
   }
@@ -76,8 +80,8 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
     this._dispose();
 
     // subscribe to onTranslationChange event, in case the translations change
-    if (!this.onTranslationChange) {
-      this.onTranslationChange = this.translate.onTranslationChange.subscribe((event: TranslationChangeEvent) => {
+    if (this._onTranslationChangeSub == null) {
+      this._onTranslationChangeSub = this.translate.onTranslationChange.subscribe((event: TranslationChangeEvent) => {
         if (this.lastKey && event.lang === this.translate.currentLang) {
           this.lastKey = null;
           this.updateValue(query, interpolateParams, event.translations);
@@ -86,8 +90,8 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
     }
 
     // subscribe to onLangChange event, in case the language changes
-    if (!this.onLangChange) {
-      this.onLangChange = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+    if (this._onLangChangeSub == null) {
+      this._onLangChangeSub = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
         if (this.lastKey) {
           this.lastKey = null; // we want to make sure it doesn't return the same value until it's been updated
           this.updateValue(query, interpolateParams, event.translations);
@@ -96,8 +100,8 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
     }
 
     // subscribe to onDefaultLangChange event, in case the default language changes
-    if (!this.onDefaultLangChange) {
-      this.onDefaultLangChange = this.translate.onDefaultLangChange.subscribe(() => {
+    if (this._onDefaultLangChangeSub == null) {
+      this._onDefaultLangChangeSub = this.translate.onDefaultLangChange.subscribe(() => {
         if (this.lastKey) {
           this.lastKey = null; // we want to make sure it doesn't return the same value until it's been updated
           this.updateValue(query, interpolateParams);
@@ -112,17 +116,14 @@ export class TranslatePipe implements PipeTransform, OnDestroy {
    * Clean any existing subscription to change events
    */
   private _dispose(): void {
-    if (typeof this.onTranslationChange !== 'undefined') {
-      this.onTranslationChange.unsubscribe();
-      this.onTranslationChange = undefined;
+    if (this._onDefaultLangChangeSub != null) {
+      this._onDefaultLangChangeSub.unsubscribe();
     }
-    if (typeof this.onLangChange !== 'undefined') {
-      this.onLangChange.unsubscribe();
-      this.onLangChange = undefined;
+    if (this._onLangChangeSub != null) {
+      this._onLangChangeSub.unsubscribe();
     }
-    if (typeof this.onDefaultLangChange !== 'undefined') {
-      this.onDefaultLangChange.unsubscribe();
-      this.onDefaultLangChange = undefined;
+    if (this._onDefaultLangChangeSub != null) {
+      this._onDefaultLangChangeSub.unsubscribe();
     }
   }
 
