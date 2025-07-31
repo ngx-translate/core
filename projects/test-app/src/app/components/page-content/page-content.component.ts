a78eaf588575d0678eee337ea0b9c5f8446e439b
Raphaël Balet <raphael.balet@bit.admin.ch>
2025-07-31 14:28:01 +0200

    docs(test-app): pipe with params example

--- projects/test-app/src/app/components/page-content/page-content.component.ts
+++ projects/test-app/src/app/components/page-content/page-content.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 import { TranslateDirective, TranslatePipe } from "@ngx-translate/core";
 import { StandaloneComponent } from "../standalone/standalone.component";
 
@@ -11,6 +14,12 @@ import { StandaloneComponent } from "../standalone/standalone.component";
         // Vendors
         TranslateDirective,
         TranslatePipe,
+
+        // Mat
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatFormFieldModule,
     ],
     templateUrl: "./page-content.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush,
